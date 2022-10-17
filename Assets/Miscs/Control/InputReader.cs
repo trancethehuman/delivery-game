a5ee8427979ca50ff475eb06fef12a696126a8e9
Hai The Dude <hainghiem375@gmail.com>
2022-10-16 23:48:00 -0400

    refactor

--- Assets/Miscs/Control/InputReader.cs
+++ Assets/Miscs/Control/InputReader.cs
@@ -8,12 +8,10 @@ public class InputReader : MonoBehaviour
 {
     private PlayerControls playerControls;
     [SerializeField] private float speed;
-    [SerializeField] private Vector3 velocity;
 
     private void Awake()
     {
         playerControls = new PlayerControls();
-        velocity = GetComponent<Rigidbody>().velocity;
     }
 
     private void OnEnable()
@@ -29,9 +27,6 @@ public class InputReader : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 move = playerControls.Player.Move.ReadValue<Vector2>();
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D))
-        {
-            GetComponent<Rigidbody>().AddForce(new Vector3(move.x * speed * Time.deltaTime, 0, move.y * speed * Time.deltaTime));
-        }
+        GetComponent<Rigidbody>().AddForce(new Vector3(move.x * speed * Time.deltaTime, 0, move.y * speed * Time.deltaTime));
     }
 }
