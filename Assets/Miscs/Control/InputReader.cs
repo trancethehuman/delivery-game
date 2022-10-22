5fe4b6665d9b77fadd80de383a1a0b9222f5a91a
Hai The Dude <hainghiem375@gmail.com>
2022-10-22 19:03:07 -0400

    working isometric controls

--- Assets/Miscs/Control/InputReader.cs
+++ Assets/Miscs/Control/InputReader.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class InputReader : MonoBehaviour
 {
     private PlayerControls playerControls;
+
     [SerializeField] private float speed;
+    [field: SerializeField] private GameObject VirtualCamera { get; set; }
+    [field: SerializeField] public float CameraRotationDegree { get; private set; }
 
     private void Awake()
     {
         playerControls = new PlayerControls();
+        SetCameraRotationDegree(VirtualCamera);
+    }
+
+    private void SetCameraRotationDegree(GameObject camera)
+    {
+        CameraRotationDegree = camera.GetComponent<Transform>().eulerAngles.y;
     }
 
     private void OnEnable()
@@ -22,7 +31,16 @@ public class InputReader : MonoBehaviour
 
     private void FixedUpdate()
     {
+        SetCameraRotationDegree(VirtualCamera);
         Vector2 move = playerControls.Player.Move.ReadValue<Vector2>();
-        GetComponent<Rigidbody>().AddForce(new Vector3(move.x * speed * Time.deltaTime, 0, move.y * speed * Time.deltaTime));
+        GetComponent<Rigidbody>().AddForce(IsometricConverter(new Vector3(move.x * speed * Time.deltaTime, 0, move.y * speed * Time.deltaTime)));
+    }
+
+    private Vector3 IsometricConverter(Vector3 inputVector3)
+    {
+        Quaternion rotation = Quaternion.Euler(0, CameraRotationDegree, 0);
+        Matrix4x4 isometricMatrix = Matrix4x4.Rotate(rotation);
+
+        return isometricMatrix.MultiplyPoint3x4(inputVector3);
     }
 }
